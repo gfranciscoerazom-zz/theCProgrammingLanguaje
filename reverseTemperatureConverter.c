b299f9ad910f91c3ecac83fc99449fecc66ab501
gfranciscoerazom <gfranciscoerazom@protonmail.com>
2021-09-07 18:20:08 -0500

    #define for magic numbers

--- reverseTemperatureConverter.c
+++ reverseTemperatureConverter.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
 	int fahr;
 
-	for(fahr = 300; fahr >= 0; fahr -= 20)
+	for(fahr = UPPER; fahr >= LOWER; fahr -= STEP)
 		printf("%3d\t%3.1f\n", fahr, (5.0/9.0) * (fahr-32));
 
 	return 0;
